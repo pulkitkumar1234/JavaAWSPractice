68bdae576c74960aa96f335ef8167f8985085488
Pulkit Kumar <47333780+pulkitkumar1234@users.noreply.github.com>
2024-08-20 00:26:11 +0530

    Update TestController.java

--- src/main/java/com/example/aws/java/java/spring/aws/TestController.java
+++ src/main/java/com/example/aws/java/java/spring/aws/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test(){
-        return "Hello From AWS";
+        return "Hello From AWS V2";
     }
 }
